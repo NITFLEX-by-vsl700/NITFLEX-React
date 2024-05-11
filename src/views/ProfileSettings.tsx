2f52422f9f2279b6133e1ec8e6c71f3e3a1e0c0c
vsl700 <51147745+vsl700@users.noreply.github.com>
2024-05-11 17:56:49 +0300

    Code fixes

--- src/views/ProfileSettings.tsx
+++ src/views/ProfileSettings.tsx
@@ -8,44 +8,48 @@ export const ProfileSettings = () => {
         <SettingsPageTemplate title="Profile Settings" additionalInfo="vsl700">
             <SettingSection title="Device sessions">
                 <table className="Settings-table">
-                    <tr>
-                        <th>Device</th>
-                        <th>Actions</th>
-                    </tr>
-                    <tr>
-                        <td>Android 12</td>
-                        <td>
-                            <button className="Delete-button nitflex-button">
-                                <img src={deleteIcon} alt="" />
-                            </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Android 12</td>
-                        <td>
-                            <button className="Delete-button nitflex-button">
-                                <img src={deleteIcon} alt="" />
-                            </button>
-                        </td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Device</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Android 12</td>
+                            <td>
+                                <button className="Delete-button nitflex-button">
+                                    <img src={deleteIcon} alt="" />
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Android 12</td>
+                            <td>
+                                <button className="Delete-button nitflex-button">
+                                    <img src={deleteIcon} alt="" />
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </SettingSection>
             <SettingSection title="Settings" separatorLine>
                 <HorizontalSetting label="Profile state">
-                    <input type="radio" name="state" id="state-active" value="active" checked />
+                    <input type="radio" name="state" id="state-active" value="active" defaultChecked />
                     <label htmlFor="state-active">✔Active</label>
                     <input type="radio" name="state" id="state-banned" value="banned" />
                     <label htmlFor="state-banned">❌Banned</label>
                 </HorizontalSetting>
                 <HorizontalSetting label="User role">
-                    <select name="role" id="role">
+                    <select name="role" id="role" defaultValue="admin">
                         <option value="owner">Owner</option>
-                        <option value="admin" selected>Admin</option>
+                        <option value="admin">Admin</option>
                         <option value="user">User</option>
                     </select>
                 </HorizontalSetting>
                 <HorizontalSetting label="Device limit">
-                    <input type="number" name="devlimit" id="device-limit" min={1} value={3} />
+                    <input type="number" name="devlimit" id="device-limit" min={1} defaultValue={3} />
                 </HorizontalSetting>
                 <button className="nitflex-button" id="btn-save">Save settings</button>
             </SettingSection>
