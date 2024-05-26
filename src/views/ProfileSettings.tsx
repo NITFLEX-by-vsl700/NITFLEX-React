fa73210b46f8319e777677bfee2bf4311e6bada4
vsl700 <51147745+vsl700@users.noreply.github.com>
2024-05-26 20:41:27 +0300

    UserSettings

--- src/views/ProfileSettings.tsx
+++ src/views/ProfileSettings.tsx
@@ -2,11 +2,64 @@ import { HorizontalSetting, SettingSection, SettingsPageTemplate } from "../comp
 import deleteIcon from '../assets/delete.svg'
 import './Settings.css'
 import './ProfileSettings.css'
+import { useEffect, useState } from "react"
+import { UserSettings, defaultUserSettings } from "../models/UserSettings"
+import axios from "axios"
+import { backendUrl } from "../globals"
 
 export const ProfileSettings = () => {
+    const [userSettings, setUserSettings] = useState(defaultUserSettings)
+
+    const getPathNameSegments = (): string[] => {
+        let result = window.location.pathname.substring(1);
+        if(result.endsWith('/'))
+            result = result.substring(0, result.lastIndexOf('/'));
+
+        return result.split('/');
+    }
+
+    const getUserId = (): string => {
+        let pathNameSegments = getPathNameSegments()
+        if(pathNameSegments[2] === undefined){
+            // return current user's id
+        }            
+
+        return getPathNameSegments()[2]
+    }
+
+    const setUserStatus = (status: string) => {
+        userSettings.status = status
+        setUserSettings(userSettings)
+    }
+
+    const setUserRole = (role: string) => {
+        userSettings.role = role
+        setUserSettings(userSettings)
+    }
+
+    const setUserDeviceLimit = (limit: number) => {
+        userSettings.deviceLimit = limit
+        setUserSettings(userSettings)
+    }
+
+    const ready = (): boolean => {
+        return userSettings !== defaultUserSettings
+    }
+
+    const onSaveSettings = () => {
+        axios.put(backendUrl + `/users/settings/${getUserId()}`, userSettings, { withCredentials: true })
+        .then(() => window.location.href = '/settings/users')
+    }
+
+    useEffect(() => {
+        axios.get(backendUrl + `/users/settings/${getUserId()}`, { withCredentials: true })
+            .then(response => response.data)
+            .then((obj: UserSettings) => setUserSettings(obj))
+    }, [])
+
     return (
         <SettingsPageTemplate title="Profile Settings" additionalInfo="vsl700">
-            <SettingSection title="Device sessions">
+            {/* <SettingSection title="Device sessions">
                 <table className="Settings-table">
                     <thead>
                         <tr>
@@ -33,26 +86,26 @@ export const ProfileSettings = () => {
                         </tr>
                     </tbody>
                 </table>
-            </SettingSection>
-            <SettingSection title="Settings" separatorLine>
+            </SettingSection> */}
+            {ready() ? <SettingSection title="Settings" separatorLine>
                 <HorizontalSetting label="Profile state">
-                    <input type="radio" name="state" id="state-active" value="active" defaultChecked />
+                    <input type="radio" name="state" id="state-active" value="ACTIVE" onChange={e => setUserStatus(e.target.value)} defaultChecked={userSettings.status === 'ACTIVE'} />
                     <label htmlFor="state-active">✔Active</label>
-                    <input type="radio" name="state" id="state-banned" value="banned" />
+                    <input type="radio" name="state" id="state-banned" value="BANNED" onChange={e => setUserStatus(e.target.value)} defaultChecked={userSettings.status === 'BANNED'} />
                     <label htmlFor="state-banned">❌Banned</label>
                 </HorizontalSetting>
                 <HorizontalSetting label="User role">
-                    <select name="role" id="role" defaultValue="admin">
-                        <option value="owner">Owner</option>
-                        <option value="admin">Admin</option>
-                        <option value="user">User</option>
+                    <select name="role" id="role" onChange={e => setUserRole(e.target.value)} defaultValue={userSettings.role}>
+                        <option value="ROLE_OWNER">Owner</option>
+                        <option value="ROLE_ADMIN">Admin</option>
+                        <option value="ROLE_USER">User</option>
                     </select>
                 </HorizontalSetting>
                 <HorizontalSetting label="Device limit">
-                    <input type="number" name="devlimit" id="device-limit" min={1} defaultValue={3} />
+                    <input type="number" name="devlimit" id="device-limit" onChange={e => setUserDeviceLimit(parseInt(e.target.value))} min={1} defaultValue={userSettings.deviceLimit} />
                 </HorizontalSetting>
-                <button className="Settings-save-button nitflex-button">Save settings</button>
-            </SettingSection>
+                <button className="Settings-save-button nitflex-button" onClick={() => onSaveSettings()}>Save settings</button>
+            </SettingSection> : <></>}
         </SettingsPageTemplate>
     )
 }
